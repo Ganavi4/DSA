a0994b4b27a84318922d0ff6df7c01f57c385a37
Ganavi M S <ganavims2006@gmail.com>
2026-05-14 15:09:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (85.49%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        // Lengths must be equal
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        // Check if goal exists in s+s
+        String doubled = s + s;
+
+        return doubled.contains(goal);
+    }
+}
